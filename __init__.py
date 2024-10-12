d48d8dea0b2fdd2c89127af43fb1cd077403a7c2
Alex Palacios <alexsebpalacios@hotmail.com>
2024-10-12 00:34:37 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from .cuadri import Cuadri
 from .cuadriD import CuadriD
 from .cuadriL import CuadriL
+from .cuadriLi import CuadriLi
 from .cuadriT import CuadriT
 from .cuadriPi import CuadriPi
 from .cuadriTPuente import CuadriTPuente
